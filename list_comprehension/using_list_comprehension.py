c057e59e7fa4978d96d7f6e5b6b5db91516519b4
Tolani99 <tolaniadisa999@gmail.com>
2024-01-18 22:49:53 +0100

    Using list comprehension

---
+++ list_comprehension/using_list_comprehension.py
@@ -0,0 +1,7 @@
+cities = ['lagos', 'ibadan', 'abeokuta']
+capitalized_cities = []
+for city in cities:
+    capitalized_cities.append(city.title())
+
+capitalized_cities = [city.title() for city in cities]
+print(capitalized_cities)
