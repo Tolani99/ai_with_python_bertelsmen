9f7ac4e7d2303ce19e0e7158a0fd6f522e86265b
Tolani99 <tolaniadisa999@gmail.com>
2024-03-20 05:03:33 +0100

    Solving the quadratic whilst taking input

--- csc_715/exercise/exercise_3/basic_sol_quadratic.py
+++ csc_715/exercise/exercise_3/basic_sol_quadratic.py
@@ -5,4 +5,10 @@ To solve a quadratic equation
 def solve_quadratic(a, b, c):
 	return ((-b + (b**2 - 4*a*c)**(1/2))/(2*a), (-b-(b**2 - 4*a*c)**(1/2))/(2*a))
 
-print(solve_quadratic(1, -5, 6))
+#print(solve_quadratic(1, -5, 6))
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+
+print(solve_quadratic(a, b, c))
